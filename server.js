46df8d04ce24035ea1d8df73f3bb01a3b7a67aab
noname01 <hjc.noname@gmail.com>
2014-08-31 23:16:04 -0700

    port change

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/site/:site", function(req, res){
   siteAnalyzer.getJson(req.params.site, res);
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port %d", server.address().port);
 });
